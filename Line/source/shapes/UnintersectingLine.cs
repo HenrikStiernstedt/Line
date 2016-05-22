c8c4c192669a1d93cb28d7d63be6088445d7f411
Henrik Stiernstedt <henrik@stiernstedt.net>
2016-05-22 19:57:27 +0200

    Omdlöpta, nya och nu publika properties.

--- Line/source/shapes/UnintersectingLine.cs
+++ Line/source/shapes/UnintersectingLine.cs
@@ -10,29 +10,31 @@ namespace Liner.source.shapes
 {
     class UnintersectingLine : System.Windows.Shapes.Shape
     {
-        private Point p1;
-        private Point p2;
+        public Point From { get; set; }
+        public Point To { get; set; }
         List<Point> points = new List<Point>();
         int counter = 0;
 
+        public LineF OriginalLine { get; set; }
         public List<LineF> LineSegments { get; set; }
         public bool HasErrors {get; set;}
 
         public UnintersectingLine(Point point1, Point point2, List<UnintersectingLine> lines)
         {
             LineSegments = new List<LineF>();
-            p1 = point1;
-            p2 = point2;
+            From = point1;
+            To = point2;
+            OriginalLine = new LineF(From, To);
 
             points.Add(point1);
             //points.Add(new Point(point1.X + 30, point2.Y - 30)); // Test. Fungerar bra att rita ut linjer som en path.
 
             // Enkel orekursiv version.
-            LineF thisline = new LineF(p1, p2);
+            LineF thisline = new LineF(From, To);
 
-            Console.Out.WriteLine("Ny linje " + p1 + " till " + p2);
+            Console.Out.WriteLine("Ny linje " + From + " till " + To);
             counter = 0;
-            LineSegments = FindPath(p1, p2, lines);
+            LineSegments = FindPath(From, To, lines);
             if (LineSegments == null)
             {
                 HasErrors = true;
@@ -56,7 +58,7 @@ namespace Liner.source.shapes
             points.Add(point2);
 
             // Registrera alla segment för framtida kollisionsdetekteringar.
-            Point lastPoint = p1;
+            Point lastPoint = From;
             for (int i = 1; i < points.Count; i++)
             {
                 LineSegments.Add(new LineF(lastPoint, points[i]));
@@ -68,21 +70,21 @@ namespace Liner.source.shapes
         public List<LineF> FindPath(Point from, Point to, List<UnintersectingLine> existingLines)
         {
             counter++;
-            if (counter == 100) return null;
+            if (counter > 20) return null;
  
             LineF thisline = new LineF(from, to);
             List<LineF> res = new List<LineF>();
 
             foreach (UnintersectingLine otherUnintersectingLine in existingLines)
             {
-                foreach (LineF otherLine in otherUnintersectingLine.LineSegments)
-                {
-                    Point? intersection = thisline.Intersection(otherLine);
+               // foreach (LineF otherLine in otherUnintersectingLine.LineSegments)
+              //  {
+                Point? intersection = thisline.Intersection(otherUnintersectingLine.OriginalLine);
                     if (intersection != null)
                     {
-                        
-                        float dFrom = (float)Math.Sqrt(Math.Pow(intersection.Value.X - otherLine.From.X, 2) + Math.Pow(intersection.Value.Y - otherLine.From.Y, 2));
-                        float dTo = (float)Math.Sqrt(Math.Pow(intersection.Value.X - otherLine.To.X, 2) + Math.Pow(intersection.Value.Y - otherLine.To.Y, 2));
+
+                        float dFrom = (float)Math.Sqrt(Math.Pow(intersection.Value.X - otherUnintersectingLine.From.X, 2) + Math.Pow(intersection.Value.Y - otherUnintersectingLine.From.Y, 2));
+                        float dTo = (float)Math.Sqrt(Math.Pow(intersection.Value.X - otherUnintersectingLine.To.X, 2) + Math.Pow(intersection.Value.Y - otherUnintersectingLine.To.Y, 2));
 
                         // Specialare! Om vi är close enough, anse at det var ok.
                         if( dFrom < 1 || dTo < 1)
@@ -94,12 +96,14 @@ namespace Liner.source.shapes
                         }
                         
                         // Vi har en korsning. Lägg till ny punkt istället. Kortast väg vinner.
-                        Point p = (dFrom < dTo ? otherLine.From : otherLine.To);
+                        // Här verkar det bli avgörande att ibland testa ett andra alternativ. 
+                        Point p = (dFrom < dTo ? otherUnintersectingLine.From : otherUnintersectingLine.To);
                         
                      //   p.X += 5;
                      //   p.Y += 5;
-                     //   Console.Out.WriteLine("Från " + from.ToString() + " till " + p.ToString());
-                     //   Console.Out.WriteLine("Sen från " + p.ToString() + " till " + to.ToString());
+                        
+                        Console.Out.WriteLine("Från " + from.ToString() + " till " + p.ToString());
+                        Console.Out.WriteLine("Sen från " + p.ToString() + " till " + to.ToString());
                         res = FindPath(from, p, existingLines);
                         if(res != null)
                         {
@@ -117,7 +121,7 @@ namespace Liner.source.shapes
                         // Keep looking.
                         continue;
                     }
-                }
+               // }
             
             }
 
@@ -136,7 +140,7 @@ namespace Liner.source.shapes
             get
             {
                 List<PathSegment> segments = new List<PathSegment>();
-                segments.Add(new LineSegment(p1, true));
+                segments.Add(new LineSegment(From, true));
                 foreach (LineF line in LineSegments)
                 {
                     segments.Add(new LineSegment(line.To, true));
@@ -149,10 +153,10 @@ namespace Liner.source.shapes
                 {
                     segments.Add(new LineSegment(p, true));
                 }
-                //segments.Add(new LineSegment(p2, true));
+                //segments.Add(new LineSegment(P2, true));
                 */
                 List<PathFigure> figures = new List<PathFigure>(1);
-                PathFigure pf = new PathFigure(p1, segments, false);
+                PathFigure pf = new PathFigure(From, segments, false);
                 figures.Add(pf);
 
                 return new PathGeometry(figures, FillRule.EvenOdd, null);
